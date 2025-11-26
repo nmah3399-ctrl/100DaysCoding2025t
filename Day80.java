a62bc0d890575812454bb6a3de3beeb584d46c39
nmah3399-ctrl <nmah3399@gmail.com>
2025-11-26 22:29:17 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.print("masukkan jumlah: ");
+        int jumlah = n.nextInt();
+        
+        int[] angka = new int[jumlah];
+        for (int i = 0; i < angka.length; i++) {
+            angka[i] = n.nextInt();
+        }   
+        System.out.println("\nIsi array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("angka:[" + i + "] ="+ angka[i]);
+        }         
+    }
+    
+}
